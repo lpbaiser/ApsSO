97159b4eb0eefd2515a87e4da6faf3a37de77651
leonardo <leonardo@baiser>
2015-11-24 22:49:26 -0200

    commit de mudanças no projeto

--- ServidorArquivos/client.c
+++ ServidorArquivos/client.c
@@ -2,75 +2,97 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
+#include <fcntl.h>
 #include "connection.h"
 
 #define BUFFER_SIZE 1024
 
-int main(int argc, char** argv){
-    
+int main(int argc, char** argv) {
+
     //Conexão ao cliente
     connection_t* connection;
-    
+
     //host do servidor e a porta a conectar
     char* host;
     char* port;
-    
+
     //Buffer usado para receber e enviar dados
     uint8_t buffer[BUFFER_SIZE];
-    
+    char typeRequest[4];
+
     //Verificar se a porta e o host foi passado como argumento
-    if(argc < 3){
+    if (argc < 3) {
         fprintf(stderr, "uso: %s host porta\n", argv[0]);
         return EXIT_FAILURE;
     }
-    
+
     //Host é o primeiro argumento e porta o segundo.
     host = argv[1];
     port = argv[2];
-    
+
     //Conectar ao servidor
-    connection = CONN_connectTo((char*)host, (char*)port);
+    connection = CONN_connectTo((char*) host, (char*) port);
 
     //verificar se houve sucesso na conexão
-    if(!connection){
+    if (!connection) {
         fprintf(stderr, "Não foi possível conectar a %s:%s!\n", host, port);
-        return(EXIT_FAILURE);
+        return (EXIT_FAILURE);
     }
-    
+
     //Aguardar a mensagem de boas vindas e imprimi-la
     CONN_receive(connection, buffer, 1024, 0);
     printf("%s\n", buffer);
-    
-    while(1){
+
+    while (1) {
         printf("Digite uma mensagem para o servidor: ");
-        
+
         //ler a mensagem a ser enviada ao servidor
         fgets(buffer, BUFFER_SIZE, stdin);
-        
+
         //subtituir \n por um terminador de string
         *(strstr(buffer, "\n")) = 0;
-        
+
         //ter certeza que há um terminador de string no último caractere
-        buffer[BUFFER_SIZE-1] = 0;
-        
+        buffer[BUFFER_SIZE - 1] = 0;
+
         //enviar a mensagem
-        CONN_send(connection, buffer, strlen(buffer)+1, 0);
+        CONN_send(connection, buffer, strlen(buffer) + 1, 0);
 
         //aguardar o echo
-        CONN_receive(connection, buffer, BUFFER_SIZE, 0);
-        printf("echo: %s\n", buffer);
-        
+        CONN_receive(connection, typeRequest, BUFFER_SIZE, 0);
+        if (strcmp(typeRequest, "ls")) {
+            CONN_receive(connection, buffer, BUFFER_SIZE, 0);
+            printf("%s\n", buffer);
+
+        } else if (strcmp(typeRequest, "wget")) {
+            char nomeArquivo[BUFFER_SIZE];
+            CONN_receive(connection, nomeArquivo, BUFFER_SIZE, 0);
+
+            FILE *f;
+            f = fopen(nomeArquivo, "w");
+
+            do {
+                
+                CONN_receive(connection, buffer, BUFFER_SIZE, 0);
+                fputc(buffer, f);
+
+            } while (strcmp(buffer, "EOF"));
+            
+            fclose(f);  
+            //guardar arquivo no diretório
+        }
+
         //verificar se enviou um "sair". Caso afirmativo, terminar o cliente.
-        if(!strcmp(buffer, "sair")){
+        if (!strcmp(buffer, "sair")) {
             break;
         }
-    }    
-    
+    }
+
     //fechar a conexão
     CONN_close(connection);
-    
+
     return EXIT_FAILURE;
-    
+
 }
 
 
--- ServidorArquivos/workerThread.c
+++ ServidorArquivos/workerThread.c
@@ -21,8 +21,8 @@
 
 void processLs(Request *r) {
     int p[2], backup;
-    char buffer[2048];
-    char buf[2048];
+    char buffer[BUFFER_SIZE];
+    char buf[BUFFER_SIZE];
 
     backup = dup(1);
     close(0);
@@ -37,6 +37,8 @@ void processLs(Request *r) {
     while (fgets(buf, 1000, stdin)) {
         strcat(buffer, buf);
     }
+    char typeRequest[2] = "ls";
+    CONN_send(r->connection, typeRequest, 2, 0);
 
     CONN_send(r->connection, buffer, BUFFER_SIZE, 0);
 }
@@ -52,7 +54,10 @@ void processWget(Request *r) {
         perror("Error opening file");
         //        return -1;
     } else {
-        c = getc(f);
+
+        CONN_send(r->connection, buffer, BUFFER_SIZE, 0);
+
+        c = fgetc(f);
         while (c != EOF) {
             strcat(buffer, c);
             if (strlen(buffer) == 2048) {
@@ -81,7 +86,7 @@ void wakeThread() {
 
     sem_wait(&full);
     pthread_mutex_lock(&mutexAddLista);
-    
+
     if (lista->size > 0) {
         request *r = (request) malloc(sizeof (request));
         if (lista->first->data->typeRequest == 1) {
@@ -92,8 +97,26 @@ void wakeThread() {
             processWget(lista->first->data);
         }
     }
-    
+
     pthread_mutex_unlock(&mutexAddLista);
 
 }
 
+char* getNameArquivo(Request *r) {
+    char nome[BUFFER_SIZE];
+    while (strlen(r->path)) {
+
+    }
+}
+
+char* inverteString(char *string) {
+    int a,b;
+    char auxiliar[strlen(string)];
+    b = strlen(string) - 1; //strlen calcula a quantidade de caracteres que tem a string
+    for (a = 0; string[a] != '\0'; a++) { //Repete enquanto nao chegar ao final da string
+        auxiliar[b] = string[a];
+        b--;
+    }
+    auxiliar[a] = '\0'; //Se nao colocar essa parte, o programa pode mostrar LIXO
+    strcpy(string, auxiliar); //Copia para a variável string o conteúdo da variável auxiliar
+}
